0b0aad993b5c68318bbed44bfe886a296fa3aa0b
Mateoo18 <mateusz.nowak.203@gmail.com>
2024-10-25 14:28:55 +0200

    Initial commit

---
+++ FuelMate/FuelMate/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for FuelMate project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FuelMate.settings')
+
+application = get_asgi_application()
