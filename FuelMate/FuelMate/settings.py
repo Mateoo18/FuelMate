a8bea84a6851d4f00575e22ca601e8dad616d30d
Mateoo18 <mateusz.nowak.203@gmail.com>
2024-10-29 19:55:14 +0100

    Commit with the connection to the database

--- FuelMate/FuelMate/settings.py
+++ FuelMate/FuelMate/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'FuelMate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Database_FuelMate_1',
+        'USER': 'root',
+        'PASSWORD': 'Projekt2024',
+        'HOST': 'database-fuelmate.cj82w604mzgq.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
