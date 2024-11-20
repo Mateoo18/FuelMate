d731c8cdaf15e92a4f060d16f70f99239dcb983b
igor <igorstudz03@gmail.com>
2024-11-20 18:38:08 +0100

    adding a logout button

--- FuelMate/accounts/urls.py
+++ FuelMate/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LogoutView
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logged_in/', views.logged_in_view, name='logged_in'),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 
 ]
