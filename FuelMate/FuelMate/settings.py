f2cdce426acc0286c15a3cbb1454df19255d31ff
igor <igorstudz03@gmail.com>
2024-12-10 18:06:08 +0100

    Map repair

--- FuelMate/FuelMate/settings.py
+++ FuelMate/FuelMate/settings.py
@@ -120,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'pl'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Warsaw'
+TIME_ZONE = 'UTP'
 
 USE_I18N = True
 
